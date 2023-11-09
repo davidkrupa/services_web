f7ea4dca7110aa4f80781aa67cbee73b7a7ebb4a
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-11-09 20:18:52 +0100

    updating icon with text component

--- components/IconWithText.jsx
+++ components/IconWithText.jsx
@@ -4,23 +4,30 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import heroImage from "../assets/heroImage.jpg";
 
-const IconWithText = ({ details: { icon, title, text } }) => {
+const IconWithText = ({ details: { title, text, image, alt, id }, path }) => {
+  const linkHref = path ? `/${path}/${id}` : `/${id}`;
+
   return (
     <motion.div
       whileHover={{ scale: 1.02, transition: { duration: 0.3 } }}
-      className="flex flex-col justify-center items-center rounded-md text-center px-4 py-6 bg-slate-100 shadow-lg"
+      className="flex flex-col justify-start items-center rounded-md h-full text-center bg-primary-50 shadow-lg"
     >
       <Image
-        src={heroImage}
+        src={image}
         width={400}
         height={400}
-        alt="work exapmle"
+        alt={alt}
         className="rounded-md shadow-md mx-auto"
       />
-      <span className="text-center text-2xl pt-4 text-orange-500">{icon}</span>
+      <div className="flex flex-col justify-start items-center gap-4 px-4 py-8">
+        {/* <span className="text-center text-2xl pt-4 text-orange-500">{icon}</span> */}
+        <h3 className="text-xl font-semibold line-clamp-1">{title}</h3>
+        <p className="line-clamp-3">{text}</p>
 
-      <h4 className="text-xl font-semibold py-4">{title}</h4>
-      <p>{text}</p>
+        <p className="text-lg text-gray-800 hover:text-primary-600 font-semibold">
+          {"Check ->"}
+        </p>
+      </div>
     </motion.div>
   );
 };
