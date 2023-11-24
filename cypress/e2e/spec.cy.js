a606242da7ce14daaad1d193eabdba6a8f940fd0
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-11-24 21:30:19 +0100

    added tests for Navbar

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,21 @@
 describe("Basic spec", () => {
-  it("Contains hero title", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:3000/");
+  });
+
+  it("Should display the navbar", () => {
+    cy.get("nav").should("exist");
+  });
+
+  it("Navbar links to sections", () => {
+    cy.get("nav a").each(($link) => {
+      const sectionId = $link.attr("href");
+      cy.get($link).click();
+      cy.url().should("include", sectionId);
+    });
+  });
+
+  it("Contains hero title", () => {
     cy.get('[data-test="hero-title"]').contains("Fotowoltaika Elektryka");
   });
 });
